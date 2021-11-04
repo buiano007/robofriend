e92b3468d34027cd08bb59c3935c3cd3f1983c0c
Alkindy <mohammedalkindy123@gmail.com>
2021-11-04 08:08:30 +0300

    Andding Redux

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware  from 'redux-thunk';
 import './index.css'; 
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './Containers/App';
+import { searchRobots, requestRobots } from './reducers';
   
+const logger = createLogger(); 
 
-ReactDOM.render(
-  <React.StrictMode>
-    < App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const rootReducer = combineReducers ({ searchRobots, requestRobots})
+const store =  createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+ 
+ReactDOM.render( 
+    <Provider store={store}>
+      <App/>
+    </Provider>, document.getElementById('root'));
 reportWebVitals();
+ 
